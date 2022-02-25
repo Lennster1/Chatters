ff9b26af176a1d8124bca74b327059d8768d3843
lennster1 <lennyluo0709@gmail.com>
2022-02-24 19:35:08 -0500

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express"); // import express
-const res = require("express/lib/response");
 const app = express(); // create express app  
 const http = require("http"); // create http server
 const server = http.createServer(app); // create http server
+const { server } = require("socket.io"); // create socket server
+const io = new Server(server); // create socket server
 
 
 
@@ -11,6 +12,11 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+  
 server.listen(3000, () => {
     console.log('listening on *:3000');
   });
